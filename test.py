11df4fffa7b768d44fd9ee1dfc7e7d919d93f0b4
mathi0101 <mathiasgonzalez0101@gmail.com>
2020-11-07 01:39:59 -0300

    test

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ from time import sleep,time
 
 #nxt.locator.make_config()
 b=nxt.locator.find_one_brick(debug=True)
-b.play_tone_and_wait(440.0, 1000) # Hace sonar el brick para verificar la conexion
+b.play_tone_and_wait(440.0, 100) # Hace sonar el brick para verificar la conexion
 
 
 
@@ -18,5 +18,4 @@ for i in range(100):
     ultrasonic.get_sample()
 stop = time()
 print 'ultrasonic latency: %s ms' % (1000 * (stop - start) / 100.0)
-
 
