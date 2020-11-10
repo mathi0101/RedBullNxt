0c264a732e540d439127a74574b1c0b0a85e29bd
mathi0101 <mathiasgonzalez0101@gmail.com>
2020-11-09 21:28:24 -0300

    update

--- run.py
+++ run.py
@@ -1,8 +1,6 @@
 
 from functions import *
 
-'''from initialize import initialize_brick_and_consts
-from initialize import b,rueda_l,rueda_r,light,color,dist_f,dis'''
 import init as v
 
 from time import sleep,time
@@ -19,15 +17,26 @@ def frenar():
     v.rueda_l.brake()
     v.rueda_r.brake()
 
+def buscarYgetColor():
+    '''
+    Esta funcion mueve el robot hacia adelante hasta que encuentra algun objeto a su izquierda o adelante.
+    Adelante:
+        Gira 180 grados hacia izq y continua buscando
+    Izquierda:
+        Detecta un objeto y va a buscarlo.
 
+    Cuando lo encuentra se empieza a acercar hasta que el sensor de color detecta un color. En este momento
+    se frena hasta que detecte bien el color y luego lo devuelve.
+    Aca termina el codigo '''
+
+    
 
 # +----------- RUN ----------------+
 
 def main():
     PATH=  'bd/colors.txt'
 
-    x=read_colors_bd(PATH,True)
-    print x
+    colors=read_colors_bd(PATH,True)
 
     #v.initialize_brick_and_consts()
 
--- tests.py
+++ tests.py
@@ -1,9 +1,3 @@
 from functions import *
 
 
-
-
-dic={'Violeta': 3,'Blanco':4,'Azul':2,'Amarillo':8}
-
-update_dic(dic)
-
